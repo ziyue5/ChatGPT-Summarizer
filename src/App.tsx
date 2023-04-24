f5e5ca934c43704e272f5378dd19e4d855b13cec
Ziyue Zhou <ziyue5@illinois.edu>
2023-04-23 23:21:57 -0500

    Implement basic layout and set up dev environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, Button, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box display={"flex"} flexDirection={"column"}>
+      <Box display={"flex"} justifyContent={"center"} sx={{ backgroundColor: "#7393B3" }} height={"50px"}>
+        <Typography variant='h5' sx={{ mt: 1.5 }}>ChatGPT Summarizer</Typography>
+      </Box>
+      <Box display={"flex"} justifyContent={"center"}>
+        <Button disableRipple variant="contained">
+          Summarize current page
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
